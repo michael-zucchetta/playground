2b00d8aabe2819682af54027ba4a1a74452108ea
michael-zucchetta <michael.zucchetta@gmail.com>
2018-09-10 20:22:23 +0000

    stuff

--- colombina/dataset/dataset.py
+++ colombina/dataset/dataset.py
@@ -1,6 +1,11 @@
 import cv2
 
 
-class Dataset()
+class Dataset():
     def __init__():
+        imagenet_data = torchvision.datasets.ImageFolder('path/to/imagenet_root/')
+        data_loader = torch.utils.data.DataLoader(imagenet_data,
+                                          batch_size=4,
+                                          shuffle=True,
+                                          num_workers=args.nThreads)
         ()
